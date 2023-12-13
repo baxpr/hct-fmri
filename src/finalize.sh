bf50a52167cc93f9075b3e03cc5cce7dddb6154f
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-12-13 08:31:51 -0600

    ready to update matlab part

---
+++ src/finalize.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo Running $(basename "${BASH_SOURCE}")
+
+cd "${out_dir}"
+
+# Zip nifti files in SPM outputs
+for d in \
+    spm_cue_orth0 \
+    spm_cue_orth1_epsi2 \
+    spm_cue_orth1_mu33 \
+    spm_cue_mu3_feedback_epsi3_orth1 \
+; do
+    gzip "${d}"/*.nii
+done
+
+# Zip unsmoothed mean fmri
+gzip wctrrfmri_mean_all.nii
+
+# Preprocessed fmri
+mkdir SWFMRI
+cp swctrrfmri?.nii SWFMRI
+gzip SWFMRI/*.nii
+
