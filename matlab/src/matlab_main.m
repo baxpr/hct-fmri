2efe3458b7e271b07ad9bf3db852e56d378be14c
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-01-28 10:23:24 -0600

    procs

--- matlab/src/matlab_main.m
+++ matlab/src/matlab_main.m
@@ -21,15 +21,15 @@ inp.wmeanfmri_nii = fullfile(inp.out_dir,['w' n e]);
 % Full runs
 for fmri_nii = {'fmri1_nii','fmri2_nii','fmri3_nii','fmri4_nii'}
 
-    if strcmp(fmri_nii,'NONE')
-        inp.(['sw' fmri_nii]) = 'NONE';
+    if strcmp(inp.(fmri_nii{1}),'NONE')
+        inp.(['sw' fmri_nii{1}]) = 'NONE';
     else
 
         % Warp
         clear job
         job.comp{1}.def = {inp.deffwd_nii};
         job.comp{2}.id.space = {which(inp.refimg_nii)};
-        job.out{1}.pull.fnames = {inp.(fmri_nii)};
+        job.out{1}.pull.fnames = {inp.(fmri_nii{1})};
         job.out{1}.pull.savedir.saveusr = {inp.out_dir};
         job.out{1}.pull.interp = 1;
         job.out{1}.pull.mask = 0;
@@ -37,12 +37,12 @@ for fmri_nii = {'fmri1_nii','fmri2_nii','fmri3_nii','fmri4_nii'}
         spm_deformations(job);
 
         % Get filename of warped image
-        [~,n,e] = fileparts(inp.(fmri_nii));
-        inp.(['w' fmri_nii]) = fullfile(inp.out_dir,['w' n e]);
+        [~,n,e] = fileparts(inp.(fmri_nii{1}));
+        inp.(['w' fmri_nii{1}]) = fullfile(inp.out_dir,['w' n e]);
 
         % Smooth
         clear matlabbatch
-        matlabbatch{1}.spm.spatial.smooth.data = {inp.(['w' fmri_nii])};
+        matlabbatch{1}.spm.spatial.smooth.data = {inp.(['w' fmri_nii{1}])};
         matlabbatch{1}.spm.spatial.smooth.fwhm = [fwhm_mm fwhm_mm fwhm_mm];
         matlabbatch{1}.spm.spatial.smooth.dtype = 0;
         matlabbatch{1}.spm.spatial.smooth.im = 0;
@@ -50,8 +50,8 @@ for fmri_nii = {'fmri1_nii','fmri2_nii','fmri3_nii','fmri4_nii'}
         spm_jobman('run',matlabbatch);
 
         % Get filename of smoothed warped image
-        [~,n,e] = fileparts(inp.(['w' fmri_nii]));
-        inp.(['sw' fmri_nii]) = fullfile(inp.out_dir,['s' n e]);
+        [~,n,e] = fileparts(inp.(['w' fmri_nii{1}]));
+        inp.(['sw' fmri_nii{1}]) = fullfile(inp.out_dir,['s' n e]);
 
     end
 
