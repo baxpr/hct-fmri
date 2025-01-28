e97f205d9ac66ca272f1858a1f5c6a27e56a7dba
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-01-28 10:43:29 -0600

    bash preproc for varying img count

--- src/copy_inputs.sh
+++ src/copy_inputs.sh
@@ -3,10 +3,18 @@
 echo Running $(basename "${BASH_SOURCE}")
 
 # Files
-cp "${fmri1_niigz}" "${out_dir}"/fmri1.nii.gz
-cp "${fmri2_niigz}" "${out_dir}"/fmri2.nii.gz
-cp "${fmri3_niigz}" "${out_dir}"/fmri3.nii.gz
-cp "${fmri4_niigz}" "${out_dir}"/fmri4.nii.gz
+if [[ -f "${fmri1_niigz}" ]]; then
+    cp "${fmri1_niigz}" "${out_dir}"/fmri1.nii.gz
+fi
+if [[ -f "${fmri2_niigz}" ]]; then
+    cp "${fmri2_niigz}" "${out_dir}"/fmri2.nii.gz
+fi
+if [[ -f "${fmri3_niigz}" ]]; then
+    cp "${fmri3_niigz}" "${out_dir}"/fmri3.nii.gz
+fi
+if [[ -f "${fmri4_niigz}" ]]; then
+    cp "${fmri4_niigz}" "${out_dir}"/fmri4.nii.gz
+fi
 cp "${fmritopup_niigz}" "${out_dir}"/fmritopup.nii.gz
 cp "${seg_niigz}" "${out_dir}"/seg.nii.gz
 cp "${icv_niigz}" "${out_dir}"/icv.nii.gz
--- src/fsl_processing.sh
+++ src/fsl_processing.sh
@@ -43,7 +43,9 @@ rm tmp.nii.gz
 echo "Motion correction"
 for n in 1 2 3 4; do
     echo "    Run ${n}"
-    mcflirt -in fmri${n} -meanvol -out rfmri${n} -plots
+    if [[ -f fmri${n}.nii.gz ]]; then
+        mcflirt -in fmri${n} -meanvol -out rfmri${n} -plots
+    done
 done
 
 echo "    Topup run"
@@ -52,29 +54,69 @@ mcflirt -in fmritopup -meanvol -out rfmritopup
 
 # Alignment between runs and overall mean fmri
 echo "Aligning runs"
-cp rfmri1_mean_reg.nii.gz rrfmri1_mean_reg.nii.gz
-cp rfmri1.nii.gz rrfmri1.nii.gz
+if [[ -f rfmri4.nii.gz ]]; then first=4; fi
+if [[ -f rfmri3.nii.gz ]]; then first=3; fi
+if [[ -f rfmri2.nii.gz ]]; then first=2; fi
+if [[ -f rfmri1.nii.gz ]]; then first=1; fi
+cp rfmri${first}_mean_reg.nii.gz rrfmri${first}_mean_reg.nii.gz
+cp rfmri${first}.nii.gz rrfmri${first}.nii.gz
+
 opts="-usesqform -searchrx -5 5 -searchry -5 5 -searchrz -5 5"
-for n in 2 3 4; do
+for n in $(seq ${first} 4); do
     echo "    Run ${n} to run 1"
-    flirt ${opts} -in rfmri${n}_mean_reg -ref rrfmri1_mean_reg \
-        -out rrfmri${n}_mean_reg -omat r${n}to1.fslmat
-    flirt -applyxfm -init r${n}to1.fslmat -in rfmri${n} -ref rrfmri1_mean_reg -out rrfmri${n}
+    if [[ -f rfmri${n}.nii.gz ]]; then
+        flirt ${opts} -in rfmri${n}_mean_reg -ref rrfmri1_mean_reg \
+            -out rrfmri${n}_mean_reg -omat r${n}to1.fslmat
+        flirt -applyxfm -init r${n}to1.fslmat -in rfmri${n} -ref rrfmri1_mean_reg -out rrfmri${n}
+    done
 done
 
 echo "    Topup run to run 1"
-flirt ${opts} -in rfmritopup_mean_reg -ref rrfmri1_mean_reg -out rrfmritopup_mean_reg
+flirt ${opts} -in rfmritopup_mean_reg -ref rrfmri${first}_mean_reg -out rrfmritopup_mean_reg
 
 echo "    Computing overall mean"
-fslmaths rrfmri1_mean_reg -add rrfmri2_mean_reg \
-    -add rrfmri3_mean_reg -add rrfmri4_mean_reg \
-    -div 4 rrfmri_mean_all
+count=0
+fslmaths rrfmri${first}_mean_reg.nii.gz -mul 0 zeroimg
+if [[ -f rrfmri1_mean_reg.nii.gz ]]; then 
+    c1="-add rrfmri1_mean_reg.nii.gz"
+    d1="rrfmri1"
+    ((count++))
+else
+    c1=""
+    d1=""
+done
+if [[ -f rrfmri2_mean_reg.nii.gz ]]; then 
+    c2="-add rrfmri2_mean_reg.nii.gz"
+    d2="rrfmri2"
+    ((count++))
+else
+    c2=""
+    d2=""
+done
+if [[ -f rrfmri3_mean_reg.nii.gz ]]; then 
+    c3="-add rrfmri3_mean_reg.nii.gz"
+    d3="rrfmri3"
+    ((count++))
+else
+    c3=""
+    d3=""
+done
+if [[ -f rrfmri4_mean_reg.nii.gz ]]; then 
+    c4="-add rrfmri4_mean_reg.nii.gz"
+    d4="rrfmri4"
+    ((count++))
+else
+    c4=""
+    d4=""
+done
 
+fslmaths zeroimg ${c1} ${c2} ${c3} ${c4} \
+    -div ${count} rrfmri_mean_all
 
 # Run topup. After this, the 'tr' prefix files always contain the data that will be further
 # processed.
 echo "Running TOPUP"
-run_topup.sh "${pedir}" rrfmri_mean_all rrfmritopup_mean_reg rrfmri1 rrfmri2 rrfmri3 rrfmri4
+run_topup.sh "${pedir}" rrfmri_mean_all rrfmritopup_mean_reg ${d1} ${d2} ${d3} ${d4}
 
 # Register corrected mean fmri to T1. biascorr is the adjusted T1 from cat12, ICV is the 
 # ICV_NATIVE resource from cat12 that is masked to only brain.
@@ -88,8 +130,10 @@ flirt -applyisoxfm "${vox_mm}" -init ctrrfmri_mean_all.fslmat -in trrfmri_mean_a
 
 # Apply coregistration to the corrected time series
 for n in 1 2 3 4; do
-    flirt -applyisoxfm "${vox_mm}" -init ctrrfmri_mean_all.fslmat \
-        -in trrfmri${n} -ref biascorr -out ctrrfmri${n}
+    if [[ -f trrfmri${n}.nii.gz ]]; then
+        flirt -applyisoxfm "${vox_mm}" -init ctrrfmri_mean_all.fslmat \
+            -in trrfmri${n} -ref biascorr -out ctrrfmri${n}
+    fi
 done
 
 # And to the topup image, for reference
