8995219e5cccf4aaa86cb82407b811f61a2efea7
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-02-11 15:48:17 -0600

    spm call args

--- src/pipeline_main.sh
+++ src/pipeline_main.sh
@@ -11,14 +11,26 @@ eprime_to_csv.py -o "${out_dir}"/eprime.csv "${out_dir}"/eprime.txt
 # FSL based motion correction, topup, registration
 fsl_processing.sh
 
-# Matlab/SPM for first level stats. Unzip .nii first
+# Unzip .nii ahead of SPM call
 gunzip "${out_dir}"/ctrrfmri?.nii.gz \
     "${out_dir}"/ctrrfmri_mean_all.nii.gz \
     "${out_dir}"/biasnorm.nii.gz \
     "${out_dir}"/y_deffwd.nii.gz
+
+# Handle missing fmri for SPM call
+extra_cmd=
+for r in 1 2 3 4; do
+    if [[ ! -f "${out_dir}/ctrrfmri${r}.nii" ]]; then 
+        extra_cmd="${extra_cmd} fmri${r}_nii NONE"
+    fi
+done
+
+# SPM call
 run_spm12.sh "${MATLAB_RUNTIME}" function matlab_entrypoint \
     hpf_sec "${hpf_sec}" \
-    fwhm_mm "${fwhm_mm}"
+    fwhm_mm "${fwhm_mm}" \
+    out_dir "${out_dir}" \
+    ${extra_cmd}
 
 # Freeview-based PDF creation
 make_pdf.sh
