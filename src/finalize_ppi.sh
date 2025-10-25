1ebff2260f7a2a23e6595f3f934c6db4a42761b3
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-10-25 12:32:37 -0500

    wrapper

---
+++ src/finalize_ppi.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+echo Running $(basename "${BASH_SOURCE}")
+
+cd "${out_dir}"
+
+# Zip nifti files in SPM outputs
+for d in spm_hct* ppi; do
+    gzip "${d}"/*.nii
+done
+
+# Zip unsmoothed mean fmri
+gzip wctrrfmri_mean_all.nii
+
+# Preprocessed fmri
+mkdir WFMRI
+cp wctrrfmri?.nii WFMRI
+gzip WFMRI/*.nii
+mkdir SWFMRI
+cp swctrrfmri?.nii SWFMRI
+gzip SWFMRI/*.nii
+
+# In yaml, capture one ROI result in full as an example: spm_hct_manppi_dAI_L
+
+# Rename PPI/VOI dir
+mv ppi PPI_VOI_WORKFILES
+
+# Grab just the PPI con images for small and coherent PPI output. Note exact
+# specific assumption about which con image is which in the first level stats code
+mkdir PPI_RESULTS
+for roi in dAI_L dAI_R vAI_L vAI_R PI_L PI_R dAmy_L dAmy_R aMCC_L aMCC_R pACC_L pACC_R sgACC_L sgACC_R; do
+    cp spm_hct_manppi_${roi}/con_0005.nii.gz \
+        PPI_RESULTS/ppicon_0005_HeartConn_${roi}.nii.gz
+    cp spm_hct_manppi_${roi}/con_0006.nii.gz \
+        PPI_RESULTS/ppicon_0006_CountingConn_${roi}.nii.gz
+    cp spm_hct_manppi_${roi}/con_0007.nii.gz \
+        PPI_RESULTS/ppicon_0007_HeartConn_plus_CountingConn_${roi}.nii.gz
+    cp spm_hct_manppi_${roi}/con_0008.nii.gz \
+        PPI_RESULTS/ppicon_0008_HeartConn_gt_CountingConn_${roi}.nii.gz
+done
+
+# Capture the atlas
+mkdir ATLAS
+cp $(dirname "${BASH_SOURCE}")/matlab/src/atlas* VOIS
+
---
+++ src/pipeline_main_ppi.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+echo Running $(basename "${BASH_SOURCE}")
+
+# Copy inputs to the working directory
+copy_inputs.sh
+
+# Convert eprime .txt to csv format
+eprime_to_csv.py -o "${out_dir}"/eprime.csv "${out_dir}"/eprime.txt
+
+# FSL based motion correction, topup, registration
+fsl_processing.sh
+
+# Unzip .nii ahead of SPM call
+gunzip "${out_dir}"/ctrrfmri?.nii.gz \
+    "${out_dir}"/ctrrfmri_mean_all.nii.gz \
+    "${out_dir}"/biasnorm.nii.gz \
+    "${out_dir}"/y_deffwd.nii.gz
+
+# Handle missing fmri for SPM call
+extra_cmd=
+for r in 1 2 3 4; do
+    if [[ ! -f "${out_dir}/ctrrfmri${r}.nii" ]]; then 
+        extra_cmd="${extra_cmd} fmri${r}_nii NONE"
+    fi
+done
+
+# SPM call
+run_spm12.sh "${MATLAB_RUNTIME}" function matlab_entrypoint_ppi \
+    hpf_sec "${hpf_sec}" \
+    fwhm_mm "${fwhm_mm}" \
+    out_dir "${out_dir}" \
+    ${extra_cmd}
+
+# Freeview-based PDF creation
+make_pdf.sh
+
+# Finalize and organize outputs
+finalize_ppi.sh
